b63949a1c37e2449350e3f15562ff1728690274e
Charles <charlesngatia.cn@gmail.com>
2026-03-29 08:23:50 +0300

    Adding discount success message

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -16,7 +16,7 @@ class CashRegister:
     def apply_discount(self):
         if self.discount > 0:
             self.total -= self.total * (self.discount / 100)
-            print(f"After the discount, the total comes to ${self.total}.")
+            print(f"After the discount, the total comes to ${self.total:g}.")
         else:
             print("There is no discount to apply.")
 
